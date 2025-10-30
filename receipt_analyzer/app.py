dda8c30beeb35ca29cba25489cd40d60dc0cf387
Preygle <mohammad.owais2004@gmail.com>
2025-10-31 02:17:58 +0530

    added base hosting

--- receipt_analyzer/app.py
+++ receipt_analyzer/app.py
@@ -191,5 +191,6 @@ def dashboard():
 
     return render_template('dashboard.html', receipts=receipts)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
